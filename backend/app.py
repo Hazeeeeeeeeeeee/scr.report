05334356ebb27d5f9e519cc98baf548bf0a9f3ab
unknown <defaguillaume>
2023-05-19 00:32:32 +0200

    fix time and guest user

--- backend/app.py
+++ backend/app.py
@@ -9,7 +9,6 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 
 player_cache = {}
 
-# Mapping of raid names to their IDs
 raid_ids = {
     'Leviathan': 'jdzvzqvk',
     'Eater_of_Worlds': '824r4ygd',
@@ -23,13 +22,11 @@ raid_ids = {
     'Vow_of_the_Disciple':' 7kj909n2',
     'Kings_Fall': '9kvlp902',
     'Root_of_Nightmares': '9d88x6ld'
-    # Add more raids here...
 }
 
 @app.route('/raid/<raid_name>')
 @cross_origin()
 def raid(raid_name):
-    # Look up the raid ID
     raid_id = raid_ids.get(raid_name)
     if not raid_id:
         return jsonify({'error': 'Invalid raid name'}), 400
@@ -40,7 +37,7 @@ def raid(raid_name):
 
     leaderboard = []
     for i, run in enumerate(data['data']['runs'], start=1):
-        player_names = [get_player_name(player['id']) for player in run['run']['players'] if player['rel'] == 'user']
+        player_names = [get_player_name(player) for player in run['run']['players']]
 
         run_time = format_duration(run['run']['times']['primary'])
 
@@ -50,35 +47,60 @@ def raid(raid_name):
             'time': run_time
         })
 
+
     return jsonify(leaderboard)
 
-def get_player_name(player_id):
-    if player_id in player_cache:
-        return player_cache[player_id]
+def get_player_name(player):
+    if player['rel'] == 'user':
+        player_id = player['id']
+        if player_id in player_cache:
+            return player_cache[player_id]
 
-    response = requests.get(f'https://www.speedrun.com/api/v1/users/{player_id}')
+        response = requests.get(f'https://www.speedrun.com/api/v1/users/{player_id}')
 
-    data = json.loads(response.text)
+        data = json.loads(response.text)
 
-    if 'data' not in data or 'names' not in data['data'] or 'international' not in data['data']['names']:
-        print(f"Error: Unable to get player name for player_id: {player_id}")
-        return "Unknown Player"
+        if 'data' not in data or 'names' not in data['data'] or 'international' not in data['data']['names']:
+            print(f"Error: Unable to get player name for player_id: {player_id}")
+            return "Unknown Player"
 
-    player_name = data['data']['names']['international']
+        player_name = data['data']['names']['international']
 
-    player_cache[player_id] = player_name
+        player_cache[player_id] = player_name
 
-    return player_name
+        return player_name
+    elif player['rel'] == 'guest':
+        return player['name']
+
 
 
 def format_duration(duration):
+    # Remove the 'PT' prefix
     duration = duration[2:]
 
-    minutes, seconds = duration.split('M')
+    hours, minutes, seconds = 0, 0, 0
 
-    seconds = seconds[:-1]
+    # Split the duration into its components
+    time_parts = duration.split('H')
+    if len(time_parts) > 1:
+        hours = int(time_parts[0])
+        duration = time_parts[1]
 
-    return f'{minutes}:{seconds}'
+    time_parts = duration.split('M')
+    if len(time_parts) > 1:
+        minutes = int(time_parts[0])
+        duration = time_parts[1]
+
+    time_parts = duration.split('S')
+    if len(time_parts) > 1:
+        seconds = int(time_parts[0])
+
+    # Format the duration as a string
+    if hours > 0:
+        return f'{hours}:{minutes:02d}:{seconds:02d}'
+    else:
+        return f'{minutes:02d}:{seconds:02d}'
+
 
 if __name__ == '__main__':
     app.run(port=5000)
